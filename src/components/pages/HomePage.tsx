53aa0b3fcb7b6b5ca337396f0926af0b91ad87ff
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-04 18:42:47 +0200

    Feat: Add images constants in /lib/constants

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@/components/ui/button"
+import Image from 'next/image'
+import { images } from '@/lib/constants'
 
 export default function HomePage() {
-
-  return (
-    <div className="">
-      <h1 className="text-3xl underline">Home</h1>
-      <Button>Click me</Button>
-    </div>
-  )
+	return (
+		<div className="flex h-screen max-h-screen">
+			<section className="remove-scrollbar container my-auto">
+				<div className="sub-container max-w-[496px]">
+					<Image
+						src={images.LOGO_FULL.path}
+						height={1000}
+						width={1000}
+						alt="patient"
+						className="mb-12 h-10 w-fit"
+					/>
+				</div>
+			</section>
+		</div>
+	)
 }
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,8 @@
+export const images = {
+  LOGO_FULL: {
+    path: '/assets/icons/logo-full.svg'
+  },
+  LOGO_ICON: {
+    path: '/assets/icons/logo-icon.svg'
+  }
+}
