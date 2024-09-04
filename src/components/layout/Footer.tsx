4880ed24ed8635670e510b2d53c5f45e45bc346a
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-04 15:56:18 +0200

    Style: Change text color for Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer className="p-5 min-h-[70px] flex flex-col items-center justify-end">
-			<small className="text-emerald-600 text-xs text-center">
+			<small className="text-green-500 text-xs text-center">
 				&copy; ARWcode 2024 for Health Care Medical Clinic. All rights reserved.
 			</small>
 		</footer>
