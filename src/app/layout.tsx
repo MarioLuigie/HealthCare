721e3a56cd4a912eed6b1fc6b0827fe3e04c48cb
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-04 12:36:39 +0200

    Install: Install clsx tailwind-merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,24 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/lib/styles/globals.css'
+import { Plus_Jakarta_Sans as FontSans } from 'next/font/google'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const fontSans = FontSans({
+	subsets: ['latin'],
+	weight: ['300', '400', '500', '600', '700'],
+	variable: '--font-sans',
+})
+
 export const metadata: Metadata = {
 	title: 'HealthCare',
-	description: 'A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.',
+	description:
+		'A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.',
 	icons: {
-    icon: "/assets/icons/logo-icon.svg",
-  },
+		icon: '/assets/icons/logo-icon.svg',
+	},
 }
 
 export default function RootLayout({
@@ -19,7 +28,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body
+				className={cn(
+					'min-h-screen bg-dark-300 font-sans antialiased',
+					fontSans.variable
+				)}
+			>
+				{children}
+			</body>
 		</html>
 	)
 }
