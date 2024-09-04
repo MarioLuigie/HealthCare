c020c7638c44a947272570535c557d132a0a4f16
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-04 20:57:31 +0200

    Feat: Add Form from Shadcn to PatientForm.tsx

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -1,8 +1,22 @@
 'use client'
 
+// modules
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm, SubmitHandler } from 'react-hook-form'
+// lib
 import { PatientformSchema, PatientFormData } from '@/lib/types/zod'
+// components
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form'
 
 export default function PatientForm() {
 	const form = useForm<PatientFormData>({
@@ -12,9 +26,31 @@ export default function PatientForm() {
 		},
 	})
 
-  const onSubmit: SubmitHandler<PatientFormData> = (data: PatientFormData) => {
+	const onSubmit: SubmitHandler<PatientFormData> = (
+		data: PatientFormData
+	) => {}
 
-  }
-
-	return <div>Patient Form</div>
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="username"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Username</FormLabel>
+							<FormControl>
+								<Input placeholder="Enter your user name here" {...field} />
+							</FormControl>
+							<FormDescription>
+								This is your public display name.
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
+	)
 }
--- src/lib/types/zod.ts
+++ src/lib/types/zod.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod'
 
 export const PatientformSchema = z.object({
-  username: z.string().min(2).max(50),
+	username: z
+		.string()
+		.min(2, { message: 'Username must be at least 2 characters.' })
+		.max(50, { message: 'Username must be at most 50 characters.' }),
 })
 
 export type PatientFormData = z.infer<typeof PatientformSchema>
