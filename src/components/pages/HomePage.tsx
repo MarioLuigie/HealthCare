53be576cfbdd471bd1ae68fe908da292ef6dab0c
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-04 17:31:46 +0200

    Feat: Add Button component from Schadcn

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 export default function HomePage() {
 
   return (
     <div className="">
       <h1 className="text-3xl underline">Home</h1>
+      <Button variant="outline">Button</Button>
     </div>
   )
 }
