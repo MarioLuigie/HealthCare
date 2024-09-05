6909c4905d9958d2ff3f0ba6993dda3e82817aed
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-05 12:49:29 +0200

    Update: Edit images constants and create icons constants

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,12 +1,26 @@
-export const images = {
+export const icons = {
   LOGO_FULL: {
-    path: '/assets/icons/logo-full.svg'
+    path: '/assets/icons/logo-full.svg',
+    alt: 'Logo'
   },
   LOGO_ICON: {
-    path: '/assets/icons/logo-icon.svg'
+    path: '/assets/icons/logo-icon.svg',
+    alt: 'Logo'
   },
+  USER_ICON: {
+    path: '/assets/icons/user.svg',
+    alt: 'User icon'
+  },
+  EMAIL_ICON: {
+    path: '/assets/icons/email.svg',
+    alt: 'Email icon'
+  }
+}
+
+export const images = {
   HOME_PAGE_IMAGE: {
-    path: "/assets/images/onboarding-img.png"
+    path: "/assets/images/onboarding-img.png",
+    alt: 'Home page image'
   }
 }
 
