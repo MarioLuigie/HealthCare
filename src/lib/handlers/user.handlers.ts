84e50820f11121f2ff96a54bacef0a6ad20c6b34
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-06 11:50:30 +0200

    Fix: Change name from PatientFormData to UserFormData for type in handleCreateUser

--- src/lib/handlers/user.handlers.ts
+++ src/lib/handlers/user.handlers.ts
@@ -1,7 +1,7 @@
-import { PatientFormData } from '@/lib/types/zod'
+import { UserFormData } from '@/lib/types/zod'
 import { createUser } from '@/lib/actions/user.actions'
 
-export const handleCreateUser = async (data: PatientFormData) => {
+export const handleCreateUser = async (data: UserFormData) => {
 	try {
 		await new Promise((resolve) => {
 			setTimeout(resolve, 2000)
