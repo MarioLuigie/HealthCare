3aa81802e8d3da8bdf07a28ca93e5bee087d245d
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-06 17:26:04 +0200

    Feat: Create RegistrationPage and PatientForm

---
+++ src/components/forms/PatientForm.tsx
@@ -0,0 +1,8 @@
+export default function PatientForm({ user }: { user: any}) {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/components/pages/RegistrationPage.tsx
+++ src/components/pages/RegistrationPage.tsx
@@ -1,8 +1,30 @@
-export default function RegistrationPage() {
+// modules
+import Image from 'next/image'
+// lib
+import { icons } from '@/lib/constants'
+// components
+import PatientForm from '@/components/forms/PatientForm'
 
-  return (
-    <div>
-      
-    </div>
-  )
+
+export default function RegistrationPage() {
+  const user = {}
+	return (
+		<div className="flex h-screen max-h-screen">
+			<section className="remove-scrollbar container">
+				<div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+					<Image
+						src={icons.LOGO_FULL.path}
+						height={1000}
+						width={1000}
+						alt={icons.LOGO_FULL.alt}
+						className="mb-12 h-10 w-fit"
+					/>
+
+					<PatientForm user={user} />
+
+					<p className="copyright py-12">© 2024 HealthCare by ARWcode</p>
+				</div>
+			</section>
+		</div>
+	)
 }
