92483b913c908ff1f851532eb326ae6714e25a3c
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-08 00:05:03 +0200

    Feat: Add validation schema for PatientForm in zod.ts

--- src/lib/types/zod.ts
+++ src/lib/types/zod.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
 
+// UserForm
 export const UserFormSchema = z.object({
 	name: z
 		.string()
@@ -12,3 +13,18 @@ export const UserFormSchema = z.object({
 })
 
 export type UserFormData = z.infer<typeof UserFormSchema>
+
+// PatientForm
+export const PatientFormSchema = z.object({
+	name: z
+		.string()
+		.min(2, 'Name must be at least 2 characters')
+		.max(50, 'Name must be at most 50 characters'),
+	email: z.string().email('Invalid email address'),
+	phone: z
+		.string()
+		.refine((phone) => /^\+\d{10,15}$/.test(phone), 'Invalid phone number'),
+	identificationType: z.string()
+})
+
+export type PatientFormData = z.infer<typeof PatientFormSchema>
