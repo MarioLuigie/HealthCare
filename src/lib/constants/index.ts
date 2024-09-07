e194774d02ef2b6920b431a9df3228b3e19b4d9d
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-07 23:48:59 +0200

    Feat: Add identification types to constants

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -39,6 +39,20 @@ export const Doctors = [
 	},
 ]
 
+export const IdentificationTypes = [
+	'Birth Certificate',
+	"Driver's License",
+	'Medical Insurance Card/Policy',
+	'Military ID Card',
+	'National Identity Card',
+	'Passport',
+	'Resident Alien Card (Green Card)',
+	'Social Security Card',
+	'State ID Card',
+	'Student ID Card',
+	'Voter ID Card',
+]
+
 export const icons = {
 	LOGO_FULL: {
 		path: '/assets/icons/logo-full.svg',
