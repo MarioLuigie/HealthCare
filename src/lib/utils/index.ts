2e8bfea5ceaf8bdcfced47c13cb12a0bb9a913db
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-09 16:47:16 +0200

    Feat: Create formatDateToYMD func in /lib/utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -68,6 +68,14 @@ export const formatDateTime = (dateString: Date | string) => {
 	}
 }
 
+export const formatDateToYMD = (dateString: Date | string): string => {
+	const date = new Date(dateString);
+	const year = date.getFullYear();
+	const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Miesiące są indeksowane od 0
+	const day = date.getDate().toString().padStart(2, '0');
+	return `${year}-${month}-${day}`;
+}
+
 export function encryptKey(passkey: string) {
 	return btoa(passkey)
 }
