e54e527f49e28b17c8a65a9d05098e3906455a6c
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-09 19:37:16 +0200

    Refactor: Add FormPageTemplate to HomePage and RegistrationPage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -8,36 +8,25 @@ import PageTitle from '@/components/shared/PageTitle'
 import UserForm from '@/components/forms/UserForm'
 import LogoFull from '@/components/content/LogoFull'
 import Copyright from '@/components/content/Copyright'
+import FormPageTemplate from '@/components/shared/FormPageTemplate'
 
 export default function HomePage() {
+	{
+		/* TODO: OTP Verification | PassKey Modal  */
+	}
 	return (
-		<div className="flex h-screen max-h-screen">
-			{/* TODO: OTP Verification | PassKey Modal  */}
-			<section className="remove-scrollbar container my-auto">
-				<div className="sub-container max-w-[496px]">
-					<LogoFull />
-					<PageTitle
-						title="Hi there !"
-						description="Get started with appointments."
-					/>
-					<UserForm />
-					<Copyright>
-						<Link href="/?admin=true" className="text-green-500">
-							Admin
-						</Link>
-					</Copyright>
-				</div>
-			</section>
-			<section className="max-w-[50%]">
-				<Image
-					src={images.HOME_PAGE_IMAGE.path}
-					height={1000}
-					width={1000}
-					alt={images.HOME_PAGE_IMAGE.alt}
-					className="side-img"
-					priority
-				/>
-			</section>
-		</div>
+		<FormPageTemplate image={images.HOME_PAGE_IMAGE}>
+			<LogoFull />
+			<PageTitle
+				title="Hi there !"
+				description="Get started with appointments."
+			/>
+			<UserForm />
+			<Copyright>
+				<Link href="/?admin=true" className="text-green-500">
+					Admin
+				</Link>
+			</Copyright>
+		</FormPageTemplate>
 	)
 }
--- src/components/pages/RegistrationPage.tsx
+++ src/components/pages/RegistrationPage.tsx
@@ -8,33 +8,21 @@ import PatientForm from '@/components/forms/PatientForm'
 import PageTitle from '@/components/shared/PageTitle'
 import LogoFull from '@/components/content/LogoFull'
 import Copyright from '@/components/content/Copyright'
+import FormPageTemplate from '@/components/shared/FormPageTemplate'
 
 export default async function RegistrationPage({ userId }: { userId: string }) {
 	const user = await getUser(userId)
 
 	console.log('User from RegistrationPage:', user)
 	return (
-		<div className="flex h-screen max-h-screen">
-			<section className="remove-scrollbar container my-auto max-h-screen overflow-y-auto">
-				<div className="sub-container max-w-[780px]">
-					<LogoFull />
-					<PageTitle
-						title="Welcome !"
-						description="Let us know more about yourself."
-					/>
-					<PatientForm user={user} />
-					<Copyright />
-				</div>
-			</section>
-			<section className="max-w-[33%]">
-				<Image
-					src={images.REGISTER_PAGE_IMAGE.path}
-					height={1000}
-					width={1000}
-					alt={images.REGISTER_PAGE_IMAGE.alt}
-					className="side-img"
-				/>
-			</section>
-		</div>
+		<FormPageTemplate image={images.REGISTER_PAGE_IMAGE}>
+			<LogoFull />
+			<PageTitle
+				title="Welcome !"
+				description="Let us know more about yourself."
+			/>
+			<PatientForm user={user} />
+			<Copyright />
+		</FormPageTemplate>
 	)
 }
