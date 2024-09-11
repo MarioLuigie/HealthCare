9e0ef9ad930ed833b40b5ce89ef38cff0e77411e
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-11 18:30:12 +0200

    Update: Add default values to PatientFormSchema

--- src/lib/types/zod.ts
+++ src/lib/types/zod.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-// UserForm
+// UserForm validation
 export const UserFormSchema = z.object({
 	name: z
 		.string()
@@ -14,77 +14,88 @@ export const UserFormSchema = z.object({
 
 export type UserFormData = z.infer<typeof UserFormSchema>
 
-// PatientForm
+// PatientForm validation
 export const PatientFormSchema = z.object({
 	name: z
 		.string()
 		.min(2, 'Name must be at least 2 characters')
-		.max(50, 'Name must be at most 50 characters'),
-	email: z.string().email('Invalid email address'),
+		.max(50, 'Name must be at most 50 characters')
+		.default(''),
+	email: z.string().email('Invalid email address').default(''),
 	phone: z
 		.string()
-		.refine((phone) => /^\+\d{10,15}$/.test(phone), 'Invalid phone number'),
+		.refine((phone) => /^\+\d{10,15}$/.test(phone), 'Invalid phone number')
+		.default(''),
 
-	birthDate: z.coerce.date(),
-	gender: z.enum(['Male', 'Female', 'Other']),
+	birthDate: z.coerce.date().default(new Date('01-01-1900')),
+	gender: z.enum(['Male', 'Female', 'Other']).default('Male' as Gender),
 	address: z
 		.string()
 		.min(5, 'Address must be at least 5 characters')
-		.max(500, 'Address must be at most 500 characters'),
+		.max(500, 'Address must be at most 500 characters')
+		.default(''),
 	occupation: z
 		.string()
 		.min(2, 'Occupation must be at least 2 characters')
-		.max(500, 'Occupation must be at most 500 characters'),
+		.max(500, 'Occupation must be at most 500 characters')
+		.default(''),
 	emergencyContactName: z
 		.string()
 		.min(2, 'Contact name must be at least 2 characters')
-		.max(50, 'Contact name must be at most 50 characters'),
+		.max(50, 'Contact name must be at most 50 characters')
+		.default(''),
 	emergencyContactNumber: z
 		.string()
 		.refine(
 			(emergencyContactNumber) =>
 				/^\+\d{10,15}$/.test(emergencyContactNumber),
 			'Invalid phone number'
-		),
-	primaryPhysician: z.string().min(2, 'Select at least one doctor'),
+		)
+		.default(''),
+	primaryPhysician: z
+		.string()
+		.min(2, 'Select at least one doctor')
+		.default(''),
 	insuranceProvider: z
 		.string()
 		.min(2, 'Insurance name must be at least 2 characters')
-		.max(50, 'Insurance name must be at most 50 characters'),
+		.max(50, 'Insurance name must be at most 50 characters')
+		.default(''),
 	insurancePolicyNumber: z
 		.string()
 		.min(2, 'Policy number must be at least 2 characters')
-		.max(50, 'Policy number must be at most 50 characters'),
-	allergies: z.string().optional(),
-	currentMedication: z.string().optional(),
-	familyMedicalHistory: z.string().optional(),
-	pastMedicalHistory: z.string().optional(),
-	identificationType: z.string().optional(),
-	identificationNumber: z.string().optional(),
-	identificationDocuments: z.custom<File[]>().optional(),
+		.max(50, 'Policy number must be at most 50 characters')
+		.default(''),
+	allergies: z.string().optional().default(''),
+	currentMedication: z.string().optional().default(''),
+	familyMedicalHistory: z.string().optional().default(''),
+	pastMedicalHistory: z.string().optional().default(''),
+	identificationType: z.string().optional().default(''),
+	identificationNumber: z.string().optional().default(''),
+	identificationDocuments: z.custom<File[]>().optional().default([]),
 	treatmentConsent: z
 		.boolean()
-		.default(false)
 		.refine((value) => value === true, {
 			message: 'You must consent to treatment in order to proceed',
-		}),
+		})
+		.default(false),
 	disclosureConsent: z
 		.boolean()
-		.default(false)
 		.refine((value) => value === true, {
 			message: 'You must consent to disclosure in order to proceed',
-		}),
+		})
+		.default(false),
 	privacyConsent: z
 		.boolean()
-		.default(false)
 		.refine((value) => value === true, {
 			message: 'You must consent to privacy in order to proceed',
-		}),
+		})
+		.default(false),
 })
 
 export type PatientFormData = z.infer<typeof PatientFormSchema>
 
-// AppointmentForm
+// AppointmentForm validation
 export const CreateAppointmentSchema = z.object({
 	primaryPhysician: z.string().min(2, 'Select at least one doctor'),
 	schedule: z.coerce.date(),
