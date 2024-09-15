1f568cac4d9917934fc4c7b0c00c2a5c0f36f694
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-15 21:38:18 +0200

    Feat: Add alert-dialog to PassKeyDialog

--- src/components/dialogs/PassKeyDialog.tsx
+++ src/components/dialogs/PassKeyDialog.tsx
@@ -1,8 +1,39 @@
-export default function PassKeyDialog() {
+'use client'
 
-  return (
-    <div>
-      PASS KEY MODAL
-    </div>
-  )
+// modules
+import { useState } from 'react'
+// components
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
+export default function PassKeyDialog() {
+  const [ isOpen, setIsOpen ] = useState<boolean>(false)
+
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger>Open</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently delete
+						your account and remove your data from our servers.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction>Continue</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	)
 }
