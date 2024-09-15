e8847659ca5e978d7475c10781ffbfb5ab24b4fa
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-15 17:39:50 +0200

    Feat: Add Link to LogoFull component

--- src/components/content/LogoFull.tsx
+++ src/components/content/LogoFull.tsx
@@ -1,16 +1,20 @@
 // modules
 import Image from 'next/image'
+import Link from 'next/link'
 // lib
 import { Icons } from '@/lib/constants'
+import { Route } from '@/lib/constants/paths'
 
 export default function LogoFull() {
 	return (
-		<Image
-			src={Icons.LOGO_FULL.path}
-			height={1000}
-			width={1000}
-			alt={Icons.LOGO_FULL.alt}
-			className="mb-12 h-10 w-fit"
-		/>
+		<Link href={Route.HOME}>
+			<Image
+				src={Icons.LOGO_FULL.path}
+				height={1000}
+				width={1000}
+				alt={Icons.LOGO_FULL.alt}
+				className="mb-12 h-10 w-fit"
+			/>
+		</Link>
 	)
 }
--- src/components/pages/SuccessApointmentPage.tsx
+++ src/components/pages/SuccessApointmentPage.tsx
@@ -1,84 +1,86 @@
 // modules
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 // lib
-import { formatDateTime } from "@/lib/utils"
+import { formatDateTime } from '@/lib/utils'
 import { doctors } from '@/lib/constants'
-import { getAppointment } from "@/lib/actions/appointment.actions"
+import { getAppointment } from '@/lib/actions/appointment.actions'
 // components
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
+import Copyright from '@/components/content/Copyright'
+import LogoFull from '../content/LogoFull'
 
 export default async function SuccessApointmentPage({
 	appointmentId,
-  userId
+	userId,
 }: {
 	appointmentId: string | string[] | undefined
-  userId: string
+	userId: string
 }) {
-  const appointment = await getAppointment(appointmentId)
+	const appointment = await getAppointment(appointmentId)
 
-  const doctor = doctors.find((doctor) => {
-    
-  })
+	const doctor = doctors.find((doctor) => {})
 
 	return (
-    <div className=" flex h-screen max-h-screen px-[5%]">
-    <div className="success-img">
-      <Link href="/">
+		<div className=" flex h-screen max-h-screen px-[5%]">
+			<div className="success-img">
+				{/* <Link href="/">
         <Image
-          src="/assets/icons/logo-full.svg"
+          src={IconPath.LOGO_FULL}
           height={1000}
           width={1000}
           alt="logo"
           className="h-10 w-fit"
         />
-      </Link>
+      </Link> */}
 
-      <section className="flex flex-col items-center">
-        <Image
-          src="/assets/gifs/success.gif"
-          height={300}
-          width={280}
-          alt="success"
-        />
-        <h2 className="header mb-6 max-w-[600px] text-center">
-          Your <span className="text-green-500">appointment request</span> has
-          been successfully submitted!
-        </h2>
-        <p>We&apos;ll be in touch shortly to confirm.</p>
-      </section>
+				<LogoFull />
 
-      <section className="request-details">
-        <p>Requested appointment details: </p>
-        <div className="flex items-center gap-3">
-          <Image
-            src={doctor?.image!}
-            alt="doctor"
-            width={100}
-            height={100}
-            className="size-6"
-          />
-          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
-        </div>
-        <div className="flex gap-2">
-          <Image
-            src="/assets/icons/calendar.svg"
-            height={24}
-            width={24}
-            alt="calendar"
-          />
-          <p> {formatDateTime(appointment.schedule).dateTime}</p>
-        </div>
-      </section>
+				<section className="flex flex-col items-center">
+					<Image
+						src="/assets/gifs/success.gif"
+						height={300}
+						width={280}
+						alt="success"
+					/>
+					<h2 className="header mb-6 max-w-[600px] text-center">
+						Your{' '}
+						<span className="text-green-500">appointment request</span>{' '}
+						has been successfully submitted!
+					</h2>
+					<p>We&apos;ll be in touch shortly to confirm.</p>
+				</section>
 
-      <Button variant="outline" className="shad-primary-btn" asChild>
-        <Link href={`/patients/${userId}/new-appointment`}>
-          New Appointment
-        </Link>
-      </Button>
+				<section className="request-details">
+					<p>Requested appointment details: </p>
+					<div className="flex items-center gap-3">
+						<Image
+							src={doctor?.image!}
+							alt="doctor"
+							width={100}
+							height={100}
+							className="size-6"
+						/>
+						<p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+					</div>
+					<div className="flex gap-2">
+						<Image
+							src="/assets/icons/calendar.svg"
+							height={24}
+							width={24}
+							alt="calendar"
+						/>
+						<p> {formatDateTime(appointment.schedule).dateTime}</p>
+					</div>
+				</section>
 
-      <p className="copyright">© 2024 CarePluse</p>
-    </div>
-  </div>
-  )
+				<Button variant="outline" className="shad-primary-btn" asChild>
+					<Link href={`/patients/${userId}/new-appointment`}>
+						New Appointment
+					</Link>
+				</Button>
+				<Copyright />
+			</div>
+		</div>
+	)
 }
