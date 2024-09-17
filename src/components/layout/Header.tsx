a598619fdb2c8f61b7fcd6382b0a61f1fbb1ed75
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-17 22:42:12 +0200

    Styles: Style Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import LogoFull from '@/components/content/LogoFull'
 
 export default function Header() {
 	return (
-		<header className="border-b-[1px] border-zinc-700 p-6 max-sm:p-4 z-40 min-h-[120px] sticky top-0 left-0 w-full grid grid-cols-3 items-center">
+		<header className="border-b-[1px] border-zinc-800 p-6 max-sm:p-4 z-40 min-h-[120px] sticky top-0 left-0 w-full grid grid-cols-3 items-center">
 			<LogoFull />
 		</header>
 	)
