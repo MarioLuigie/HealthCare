d7366f18b379df556b3d98145d1df7c64b0ae656
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-09-19 20:56:53 +0200

    Feat: Add path params in Header

--- src/app/(root)/dashboard/[role]/[id]/layout.tsx
+++ src/app/(root)/dashboard/[role]/[id]/layout.tsx
@@ -1,11 +1,17 @@
-import { Header, Main, Footer } from '@/components/layout'
+import { Header, Main, Footer } from "@/components/layout"
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="flex flex-col justify-between min-h-screen">
-			<Header />
-			<Main>{children}</Main>
-			<Footer />
-		</div>
-	)
+export default function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: SingleSlugParams
+}) {
+  return (
+    <div className="flex flex-col justify-between min-h-screen">
+      <Header params={params} />
+      <Main>{children}</Main>
+      <Footer />
+    </div>
+  )
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,12 @@
 import LogoFull from "@/components/content/LogoFull"
 
-export default function Header() {
+export default function Header({ params }: { params: SingleSlugParams}) {
   return (
     <header className="border-b-[1px] border-zinc-800 p-6 max-sm:p-4 z-40 min-h-[90px] sticky top-0 left-0 w-full grid grid-cols-3 items-center">
       <LogoFull />
       <div className="flex"></div>
       <div className="flex-end">
-        <div>Admin</div>
+        <div>{params.role}</div>
       </div>
     </header>
   )
