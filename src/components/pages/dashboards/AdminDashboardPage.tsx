06502b2a67e90080c946a308270ea791bc202d18
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-09-20 17:53:17 +0200

    Feat: Add total appointments in AdminDashboardPage

--- src/components/pages/dashboards/AdminDashboardPage.tsx
+++ src/components/pages/dashboards/AdminDashboardPage.tsx
@@ -24,12 +24,21 @@ export default async function AdminDashboardPage({
 
 	return (
 		<div className="flex flex-col items-center justify-start grow p-6">
+			<p className="w-full text-lg mb-4">
+				Total Appointments: {appointments.totalCount}
+			</p>
 			<section className="admin-stat">
 				<StateCard
 					status={Status.SCHEDULED}
 					count={appointments.scheduledCount}
 					label={`${Status.SCHEDULED} Appointments`}
 					icon={Icons.SCHEDULED_ICON}
+				/>
+				<StateCard
+					status={Status.FINISHED}
+					count={appointments.finishedCount}
+					label={`${Status.FINISHED} Appointments`}
+					icon={Icons.FINISHED_ICON}
 				/>
 				<StateCard
 					status={Status.PENDING}
@@ -43,19 +52,13 @@ export default async function AdminDashboardPage({
 					label={`${Status.CANCELLED} Appointments`}
 					icon={Icons.CANCELLED_ICON}
 				/>
-				<StateCard
-					status={Status.FINISHED}
-					count={appointments.finishedCount}
-					label={`${Status.FINISHED} Appointments`}
-					icon={Icons.FINISHED_ICON}
-				/>
 			</section>
-			<div>
+			{/* <div>
 				<p>{role.toUpperCase()} DASHBOARD PAGE.</p>
 				<p className="text-sm text-dark-600">
 					{role.toUpperCase()} ID: {id}
 				</p>
-			</div>
+			</div> */}
 		</div>
 	)
 }
