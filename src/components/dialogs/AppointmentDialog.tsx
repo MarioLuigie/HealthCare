bfff7300ad28f76f0a702fcf04c6d4ee7bad78f8
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-09-23 01:19:46 +0200

    Feat: Create AppointmentDialog

---
+++ src/components/dialogs/AppointmentDialog.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+//modules
+import { useState } from "react"
+// components
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+export default function AppointmentDialog() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
+}
