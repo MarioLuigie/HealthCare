1e2ea63fef94f2dd87c89e60c9402315e81f522a
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-09-23 19:44:13 +0200

    Add StatusItem as children to AppointmentDialog in AdminDropDownMenuRow

--- src/components/content/tables/admin/AdminDropDownMenuRow.tsx
+++ src/components/content/tables/admin/AdminDropDownMenuRow.tsx
@@ -1,122 +1,125 @@
-"use client"
+'use client'
 
 // modules
-import { MoreHorizontal } from "lucide-react"
-import { useParams } from "next/navigation"
-import Image from "next/image"
+import { MoreHorizontal } from 'lucide-react'
+import { useParams } from 'next/navigation'
+import Image from 'next/image'
 //lib
-import { Icons, StatusConfig } from "@/lib/constants"
-import { Status } from "@/lib/types/enums"
+import { Icons, StatusConfig } from '@/lib/constants'
+import { Status } from '@/lib/types/enums'
 import {
-  handleAwaitAppointment,
-  handleCancelAppointment,
-  handleFinishAppointment,
-  handleScheduleAppointment,
-} from "@/lib/handlers/appointment.handlers"
+	handleAwaitAppointment,
+	handleCancelAppointment,
+	handleFinishAppointment,
+	handleScheduleAppointment,
+} from '@/lib/handlers/appointment.handlers'
 //components
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import AppointmentDialog from '@/components/dialogs/AppointmentDialog'
 
 const StatusItem = ({
-  status,
-  title,
-  icon,
+	status,
+	title,
+	icon,
 }: {
-  status: Status
-  title: string
-  icon: { path: string; alt: string }
+	status: Status
+	title: string
+	icon: { path: string; alt: string }
 }): React.ReactNode => {
-  return (
-    <div className="flex gap-2 items-center">
-      <Image
-        src={icon.path}
-        width={25}
-        height={25}
-        alt={icon.alt}
-        className="w-4"
-      />
-      <p className={StatusConfig[status].textColor}>{title}</p>
-    </div>
-  )
+	return (
+		<div className="flex gap-2 items-center">
+			<Image
+				src={icon.path}
+				width={25}
+				height={25}
+				alt={icon.alt}
+				className="w-4"
+			/>
+			<p className={StatusConfig[status].textColor}>{title}</p>
+		</div>
+	)
 }
 
 export default function AdminDropDownMenuRow({ row }: { row: any }) {
-  const params = useParams() as SingleSlugParams
-  const appointment = row.original
+	const params = useParams() as SingleSlugParams
+	const appointment = row.original
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
-          <MoreHorizontal className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-          onClick={() => navigator.clipboard.writeText(appointment.$id)}
-          className="cursor-pointer"
-        >
-          Copy appointment ID
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="cursor-pointer"
-          onClick={() => handleScheduleAppointment(appointment, params)}
-          disabled={appointment.status === Status.SCHEDULED}
-        >
-          <StatusItem
-            status={Status.SCHEDULED}
-            title="Shedule"
-            icon={Icons.SCHEDULED_ICON}
-          />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="cursor-pointer"
-          onClick={() => handleCancelAppointment(appointment, params)}
-          disabled={appointment.status === Status.CANCELLED}
-        >
-          <StatusItem
-            status={Status.CANCELLED}
-            title="Cancel"
-            icon={Icons.CANCELLED_ICON}
-          />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="cursor-pointer"
-          onClick={() => handleAwaitAppointment(appointment, params)}
-          disabled={appointment.status === Status.PENDING}
-        >
-          <StatusItem
-            status={Status.PENDING}
-            title="Pend"
-            icon={Icons.PENDING_ICON}
-          />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="cursor-pointer"
-          onClick={() => handleFinishAppointment(appointment, params)}
-          disabled={appointment.status === Status.FINISHED}
-        >
-          <StatusItem
-            status={Status.FINISHED}
-            title="Finish"
-            icon={Icons.FINISHED_ICON}
-          />
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer">
-          View appointment details
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer">
-          View patient details
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="ghost" className="h-8 w-8 p-0">
+					<MoreHorizontal className="h-4 w-4" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuItem
+					onClick={() => navigator.clipboard.writeText(appointment.$id)}
+					className="cursor-pointer"
+				>
+					Copy appointment ID
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem
+					className="cursor-pointer"
+					onClick={() => handleScheduleAppointment(appointment, params)}
+					disabled={appointment.status === Status.SCHEDULED}
+				>
+					<AppointmentDialog appointment={appointment} params={params}>
+						<StatusItem
+							status={Status.SCHEDULED}
+							title="Shedule"
+							icon={Icons.SCHEDULED_ICON}
+						/>
+					</AppointmentDialog>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					className="cursor-pointer"
+					onClick={() => handleCancelAppointment(appointment, params)}
+					disabled={appointment.status === Status.CANCELLED}
+				>
+					<StatusItem
+						status={Status.CANCELLED}
+						title="Cancel"
+						icon={Icons.CANCELLED_ICON}
+					/>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					className="cursor-pointer"
+					onClick={() => handleAwaitAppointment(appointment, params)}
+					disabled={appointment.status === Status.PENDING}
+				>
+					<StatusItem
+						status={Status.PENDING}
+						title="Pend"
+						icon={Icons.PENDING_ICON}
+					/>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					className="cursor-pointer"
+					onClick={() => handleFinishAppointment(appointment, params)}
+					disabled={appointment.status === Status.FINISHED}
+				>
+					<StatusItem
+						status={Status.FINISHED}
+						title="Finish"
+						icon={Icons.FINISHED_ICON}
+					/>
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem className="cursor-pointer">
+					View appointment details
+				</DropdownMenuItem>
+				<DropdownMenuItem className="cursor-pointer">
+					View patient details
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
 }
--- src/components/dialogs/AppointmentDialog.tsx
+++ src/components/dialogs/AppointmentDialog.tsx
@@ -1,31 +1,44 @@
 'use client'
 
 //modules
-import { useState } from "react"
+import { useState } from 'react'
 // components
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog'
+// lib
+import { Appointment } from '@/lib/types/appwrite.types'
 
-export default function AppointmentDialog() {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  )
+type AppointmentDialogProps = {
+	children: React.ReactNode
+	appointment: Appointment
+	params: SingleSlugParams
+}
+
+export default function AppointmentDialog({
+	children,
+	appointment,
+	params,
+}: AppointmentDialogProps) {
+	const [isOpen, setIsOpen] = useState<boolean>(false)
+
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogTrigger>{children}</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Are you absolutely sure?</DialogTitle>
+					<DialogDescription>
+						This action cannot be undone. This will permanently delete
+						your account and remove your data from our servers.
+					</DialogDescription>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	)
 }
