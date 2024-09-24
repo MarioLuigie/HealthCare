08f9e21869ce02a2598aa97806cfb1c89409eee8
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-09-24 18:29:40 +0200

    Feat: Add title for appointments table in AdminDashboardPage

--- src/components/pages/dashboards/AdminDashboardPage.tsx
+++ src/components/pages/dashboards/AdminDashboardPage.tsx
@@ -56,6 +56,7 @@ export default async function AdminDashboardPage({
 
 			{/* Data Table */}
 			<section className="w-full">
+        <p className='mb-4 text-lg'>Appointments List</p>
 				<DataTable columns={adminColumns} data={appointments.documents} />
 			</section>
 		</div>
