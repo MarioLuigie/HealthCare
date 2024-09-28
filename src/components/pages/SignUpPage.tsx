685b09420463f9379d008182d5ea7fafd4520430
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-09-28 17:13:02 +0200

    Styles: Style SignUpPage

--- src/components/pages/SignUpPage.tsx
+++ src/components/pages/SignUpPage.tsx
@@ -1,34 +1,38 @@
 // modules
-import Link from 'next/link'
+import Link from "next/link"
 // lib
-import { Images } from '@/lib/constants'
-import { Route } from '@/lib/constants/paths'
-import { generateUrl } from '@/lib/utils'
-import { SearchParamsString } from '@/lib/types/enums'
+import { Images } from "@/lib/constants"
+import { Route } from "@/lib/constants/paths"
+import { generateUrl } from "@/lib/utils"
+import { SearchParamsString } from "@/lib/types/enums"
 // components
-import PageTitle from '@/components/shared/PageTitle'
-import UserForm from '@/components/forms/UserForm'
-import LogoFull from '@/components/content/LogoFull'
-import Copyright from '@/components/content/Copyright'
-import FormPageTemplate from '@/components/shared/FormPageTemplate'
+import PageTitle from "@/components/shared/PageTitle"
+import UserForm from "@/components/forms/UserForm"
+import LogoFull from "@/components/content/LogoFull"
+import Copyright from "@/components/content/Copyright"
+import FormPageTemplate from "@/components/shared/FormPageTemplate"
 // import PassKeyDialog from '@/components/dialogs/PassKeyDialog'
 
 export default function SignUpPage({
-	searchParams,
+  searchParams,
 }: {
-	searchParams?: SearchParams
+  searchParams?: SearchParams
 }) {
-	return (
-		<FormPageTemplate image={Images.HOME_PAGE_IMAGE} classes="max-w-[480px]">
-			{/* <PassKeyDialog searchParams={searchParams} /> */}
-			<LogoFull />
-			<PageTitle
-				title="Sign Up"
-				description="Feel free to register for the app."
-				classes="my-12"
-			/>
-			<UserForm />
-			<Copyright />
-		</FormPageTemplate>
-	)
+  return (
+    <FormPageTemplate image={Images.HOME_PAGE_IMAGE} classes="max-w-[480px]">
+      {/* <PassKeyDialog searchParams={searchParams} /> */}
+      <div className="flex flex-col justify-between grow">
+        <LogoFull />
+        <div>
+          <PageTitle
+            title="Sign Up"
+            description="Feel free to register for the app."
+            classes="my-12"
+          />
+          <UserForm />
+        </div>
+        <Copyright />
+      </div>
+    </FormPageTemplate>
+  )
 }
