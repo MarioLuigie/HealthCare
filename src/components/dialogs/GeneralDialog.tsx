6e55f532971bc6bccbe9ce0935a27226719de196
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-10 22:59:57 +0200

    Refactor

--- src/components/dialogs/GeneralDialog.tsx
+++ src/components/dialogs/GeneralDialog.tsx
@@ -29,7 +29,7 @@ export default function GeneralDialog({
 		<Dialog open={isOpen} onOpenChange={handleCloseDialog}>
 			<DialogContent className="border-dark-500 bg-background h-full w-full max-w-none p-0 sm:msx-w-md grid-rows-[auto,1fr]">
 				<DialogHeader className="border-dark-500 border-b p-8">
-					<DialogTitle className="mb-4">{title}</DialogTitle>
+					<DialogTitle className="mb-3 text-xl">{title}</DialogTitle>
 					<DialogDescription>{description}</DialogDescription>
 				</DialogHeader>
 				<div className="p-8 w-full flex justify-center overflow-auto">{children}</div>
