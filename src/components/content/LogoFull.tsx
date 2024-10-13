2c4e91a18829061e6f2568c6c6955799c6043781
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-14 00:19:47 +0200

    Refactor

--- src/components/content/LogoFull.tsx
+++ src/components/content/LogoFull.tsx
@@ -1,3 +1,63 @@
+// "use client"
+// // modules
+// import Image from "next/image"
+// import Link from "next/link"
+// import { useTheme } from "next-themes"
+// // lib
+// import { Icons } from "@/lib/constants"
+// import { Route } from "@/lib/constants/paths"
+// import { useEffect, useState } from "react"
+
+// export default function LogoFull({
+//   redirect = false,
+// }: {
+//   redirect?: boolean
+// }) {
+//   const { theme, resolvedTheme } = useTheme() // Use resolvedTheme for consistent theme handling
+//   const [isThemeLoaded, setIsThemeLoaded] = useState(false) // State to check if theme is loaded
+
+//   useEffect(() => {
+//     setIsThemeLoaded(true) // Set theme loaded to true once the component is mounted
+//   }, [])
+
+//   // Determine the logo based on the resolved theme
+//   const isDarkTheme = resolvedTheme === "dark"
+
+//   // Prevent rendering until theme is loaded
+//   if (!isThemeLoaded) return null;
+
+//   const logoFullPath = isDarkTheme ? Icons.LOGO_FULL_BLACK_ICON.path : Icons.LOGO_FULL_ICON.path
+//   const logoFullAlt = isDarkTheme ? Icons.LOGO_FULL_BLACK_ICON.alt : Icons.LOGO_FULL_ICON.alt
+
+//   return (
+//     <div className="h-10 w-fit hidden sm:block">
+//       {redirect ? (
+//         <Link href={Route.HOME}>
+//           <Image
+//             src={logoFullPath}
+//             height={1000}
+//             width={1000}
+//             alt={logoFullAlt}
+//             className="h-10 w-fit"
+//             priority
+//           />
+//         </Link>
+//       ) : (
+//         <Image
+//           src={logoFullPath}
+//           height={1000}
+//           width={1000}
+//           alt={logoFullAlt}
+//           className="h-10 w-fit"
+//           priority
+//         />
+//       )}
+//     </div>
+//   )
+// }
+
+
+
 "use client"
 // modules
 import Image from "next/image"
