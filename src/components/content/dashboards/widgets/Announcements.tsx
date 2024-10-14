9e67088150dbb3429c722f34cdef2d1bb08b7d48
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-14 10:42:36 +0200

    Feat: Create widgets for dashboards

---
+++ src/components/content/dashboards/widgets/Announcements.tsx
@@ -0,0 +1,13 @@
+export default function Announcements() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// REMEMBER
+
+// ! Every single one widget should be flexible and movable
+// ! Every one 50% transparent background
