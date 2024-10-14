ef1180d4fac5f35ff7e5ab2f64b56fbbd57902a8
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-14 22:52:01 +0200

    Refactor

--- src/components/content/LogoFull.tsx
+++ src/components/content/LogoFull.tsx
@@ -1,114 +1,3 @@
-// "use client"
-// // modules
-// import Image from "next/image"
-// import Link from "next/link"
-// import { useTheme } from "next-themes"
-// // lib
-// import { Icons } from "@/lib/constants"
-// import { Route } from "@/lib/constants/paths"
-// import { useEffect, useState } from "react"
-
-// export default function LogoFull({
-//   redirect = false,
-// }: {
-//   redirect?: boolean
-// }) {
-//   const { theme, resolvedTheme } = useTheme() // Use resolvedTheme for consistent theme handling
-//   const [isThemeLoaded, setIsThemeLoaded] = useState(false) // State to check if theme is loaded
-
-//   useEffect(() => {
-//     setIsThemeLoaded(true) // Set theme loaded to true once the component is mounted
-//   }, [])
-
-//   // Determine the logo based on the resolved theme
-//   const isDarkTheme = resolvedTheme === "dark"
-
-//   // Prevent rendering until theme is loaded
-//   if (!isThemeLoaded) return null;
-
-//   const logoFullPath = isDarkTheme ? Icons.LOGO_FULL_BLACK_ICON.path : Icons.LOGO_FULL_ICON.path
-//   const logoFullAlt = isDarkTheme ? Icons.LOGO_FULL_BLACK_ICON.alt : Icons.LOGO_FULL_ICON.alt
-
-//   return (
-//     <div className="h-10 w-fit hidden sm:block">
-//       {redirect ? (
-//         <Link href={Route.HOME}>
-//           <Image
-//             src={logoFullPath}
-//             height={1000}
-//             width={1000}
-//             alt={logoFullAlt}
-//             className="h-10 w-fit"
-//             priority
-//           />
-//         </Link>
-//       ) : (
-//         <Image
-//           src={logoFullPath}
-//           height={1000}
-//           width={1000}
-//           alt={logoFullAlt}
-//           className="h-10 w-fit"
-//           priority
-//         />
-//       )}
-//     </div>
-//   )
-// }
-
-// "use client"
-// // modules
-// import Image from "next/image"
-// import Link from "next/link"
-// import { useTheme } from "next-themes"
-// // lib
-// import { Icons } from "@/lib/constants"
-// import { Route } from "@/lib/constants/paths"
-
-// export default function LogoFull({
-//   redirect = false,
-// }: {
-//   redirect?: boolean
-// }) {
-//   const { theme } = useTheme()
-//   const isDarkTheme = theme === "dark"
-
-//   const logoFull = (
-//     <Image
-//       src={Icons.LOGO_FULL_ICON.path}
-//       height={1000}
-//       width={1000}
-//       alt={Icons.LOGO_FULL_ICON.alt}
-//       className="h-10 w-fit"
-//       priority
-//     />
-//   )
-//   const logoFullBlack = (
-//     <Image
-//       src={Icons.LOGO_FULL_BLACK_ICON.path}
-//       height={1000}
-//       width={1000}
-//       alt={Icons.LOGO_FULL_BLACK_ICON.alt}
-//       className="h-10 w-fit"
-//       priority
-//     />
-//   )
-
-//   return (
-//     <div className="h-10 w-fit hidden sm:block">
-//       {redirect ? (
-//         <Link href={Route.HOME}>
-//           {isDarkTheme ? logoFull : logoFullBlack}
-//         </Link>
-//       ) : isDarkTheme ? (
-//         logoFull
-//       ) : (
-//         logoFullBlack
-//       )}
-//     </div>
-//   )
-// }
-
 'use client'
 // modules
 import Image from 'next/image'
@@ -179,3 +68,116 @@ export default function LogoFull({ redirect = false, mini }: { redirect?: boolea
 		</>
 	)
 }
+
+// "use client"
+// // modules
+// import Image from "next/image"
+// import Link from "next/link"
+// import { useTheme } from "next-themes"
+// // lib
+// import { Icons } from "@/lib/constants"
+// import { Route } from "@/lib/constants/paths"
+
+// export default function LogoFull({
+//   redirect = false,
+// }: {
+//   redirect?: boolean
+// }) {
+//   const { theme } = useTheme()
+//   const isDarkTheme = theme === "dark"
+
+//   const logoFull = (
+//     <Image
+//       src={Icons.LOGO_FULL_ICON.path}
+//       height={1000}
+//       width={1000}
+//       alt={Icons.LOGO_FULL_ICON.alt}
+//       className="h-10 w-fit"
+//       priority
+//     />
+//   )
+//   const logoFullBlack = (
+//     <Image
+//       src={Icons.LOGO_FULL_BLACK_ICON.path}
+//       height={1000}
+//       width={1000}
+//       alt={Icons.LOGO_FULL_BLACK_ICON.alt}
+//       className="h-10 w-fit"
+//       priority
+//     />
+//   )
+
+//   return (
+//     <div className="h-10 w-fit hidden sm:block">
+//       {redirect ? (
+//         <Link href={Route.HOME}>
+//           {isDarkTheme ? logoFull : logoFullBlack}
+//         </Link>
+//       ) : isDarkTheme ? (
+//         logoFull
+//       ) : (
+//         logoFullBlack
+//       )}
+//     </div>
+//   )
+// }
+
+// "use client"
+// // modules
+// import Image from "next/image"
+// import Link from "next/link"
+// import { useTheme } from "next-themes"
+// // lib
+// import { Icons } from "@/lib/constants"
+// import { Route } from "@/lib/constants/paths"
+// import { useEffect, useState } from "react"
+
+// export default function LogoFull({
+//   redirect = false,
+// }: {
+//   redirect?: boolean
+// }) {
+//   const { theme, resolvedTheme } = useTheme() // Use resolvedTheme for consistent theme handling
+//   const [isThemeLoaded, setIsThemeLoaded] = useState(false) // State to check if theme is loaded
+
+//   useEffect(() => {
+//     setIsThemeLoaded(true) // Set theme loaded to true once the component is mounted
+//   }, [])
+
+//   // Determine the logo based on the resolved theme
+//   const isDarkTheme = resolvedTheme === "dark"
+
+//   // Prevent rendering until theme is loaded
+//   if (!isThemeLoaded) return null;
+
+//   const logoFullPath = isDarkTheme ? Icons.LOGO_FULL_BLACK_ICON.path : Icons.LOGO_FULL_ICON.path
+//   const logoFullAlt = isDarkTheme ? Icons.LOGO_FULL_BLACK_ICON.alt : Icons.LOGO_FULL_ICON.alt
+
+//   return (
+//     <div className="h-10 w-fit hidden sm:block">
+//       {redirect ? (
+//         <Link href={Route.HOME}>
+//           <Image
+//             src={logoFullPath}
+//             height={1000}
+//             width={1000}
+//             alt={logoFullAlt}
+//             className="h-10 w-fit"
+//             priority
+//           />
+//         </Link>
+//       ) : (
+//         <Image
+//           src={logoFullPath}
+//           height={1000}
+//           width={1000}
+//           alt={logoFullAlt}
+//           className="h-10 w-fit"
+//           priority
+//         />
+//       )}
+//     </div>
+//   )
+// }
+
+
