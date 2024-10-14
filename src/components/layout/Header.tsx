64e120856b257a253e3a2cbec2edac3f0f890c5d
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-14 17:13:19 +0200

    Refactor

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,12 +13,6 @@ export default async function Header() {
 	}
 	const breakPoint = 'lg'
 	const sessionUser = await auth.getSessionUser()
-	// let roleUser =>
-	// if role === patient => roleUser = await getPatient(sessionUser.userId) or (sessionUser.$id)
-	// or
-	// if role === doctor => roleUser = await getDoctor(sessionUser.userId) or (sessionUser.$id)
-
-	// <UserAvatar user={roleUser || sessionUser} />
 
 	return (
 		<header className="backdrop-blur-md bg-base-200/50 border-b-[1px] border-zinc-800 p-2 sm:p-6 max-sm:p-4 z-40 min-h-[90px] sticky top-0 left-0 w-full flex justify-between items-center">
