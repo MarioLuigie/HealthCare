94a5f49367d5c8ec8cd56ef0b98fba454d948aac
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-15 13:12:59 +0200

    Refactor

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,9 +25,6 @@ export default async function Header() {
 	)
 }
 
-
-
-
 {
 	/**
 			<header className="backdrop-blur-md bg-base-200/50 border-b-[1px] border-zinc-800 p-2 sm:p-6 max-sm:p-4 z-40 min-h-[90px] sticky top-0 left-0 w-full grid grid-cols-3 items-center">
