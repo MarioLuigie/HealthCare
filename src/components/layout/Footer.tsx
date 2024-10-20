4546ab139b70dbe0f00f3bec0678e63eb031b9e2
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-20 19:53:22 +0200

    Update: Rebuild Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import WidgetsSheet from "@/components/content/dashboards/widgets/WidgetsSheet"
 export default function Footer() {
   return (
     <footer className="min-h-minFooterHeight border-t border-t-border">
-      <div className="relative flex-center w-full h-full">
+      <div className="relative flex-start w-full h-full p-4">
         <Copyright />
         <WidgetsSheet className="absolute right-0 bottom-0 h-footerHeight" />
       </div>
